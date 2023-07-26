a4fdf99d9464993c893ca804bc0743646d5bfc20
kurashz <vladislav.kyrashev@gmail.com>
2023-07-26 23:31:38 +0300

    Продолжать с урока 13

---
+++ 11. Переменные и значения.js
@@ -0,0 +1,27 @@
+let name = 'Антон';
+
+/* 
+let - ключевое слово объявления
+name - название переменной
+= - присваивание
+'Антон' - значение
+*/
+
+// Наименование переменных
+
+let userName = 'Вася';
+let myClass8 = 8;
+let $myVar = 'var';
+let _name = 'Антон';
+
+// Перепресвоение переменных
+let a = 1;
+console.log(a);
+a = 'test';
+console.log(a);
+
+// Нет
+const b = 1;
+console.log(b);
+b = 'test';
+console.log(b);
---
+++ 12. Арифметические операторы.js
@@ -0,0 +1,34 @@
+/* 
+Оператор - внутрення функция js
+Операторы присваивания 
+Операторы сравнения 
+Арифметические операторы 
+Битовые операторы 
+Логические операторы
+Строковые операторы
+Тернарные операторы
+Оператор запятая
+Унарные операторы
+Операторы отношения
+*/
+
+// Базовые операторы 
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth1 = width - 4;
+const newWidth2 = width + 4;
+const newWidth3 = newWidth2 / newWidth1;
+
+// Оператор степени
+
+const volume = 2 ** 3; // 2 * 2 * 2
+console.log(volume);
+
+// Строки
+
+const city = 'Moscow';
+const street = 'K1509';
+const adress = city + ' ' +  street;
+console.log(adress);
